b8beb4a1ec69c0d91af47b2ec96e469561b70693
Andres Ramirez <85534471+AndresRamirez9912@users.noreply.github.com>
2022-12-28 08:32:01 -0500

    Add serverless offline feature (#3)
    * Add serverless offline feature
    
    * Resolve discussions

--- README.md
+++ README.md
@@ -14,6 +14,34 @@ npm install -g serverless
 serverless config credentials --provider aws --key YOUR_KEY --secret YOUR_SECRET
 ```
 
+## Local Development
+
+### Install dependencies
+
+```
+yarn install
+```
+
+### Migrate local dynamodb
+
+```
+sls dynamodb install
+```
+
+### Run serverless offline
+
+In order to run a local version of the project (API Gateway + Lambda + DynamoDB), execute the following command:
+
+```
+sls offline start
+```
+
+Now, you can test the endpoints available on the local server:
+
+```
+http://localhost:3000/
+```
+
 ## Testing
 
 ```
@@ -25,11 +53,13 @@ yarn test
 In order to deploy, you need to run the following command:
 
 ### Stage: dev
+
 ```
 yarn deploy
 ```
 
 ### Stage: prod
+
 ```
 yarn deploy:prod
 ```
--- src/lambdas/createTickEntry.js
+++ src/lambdas/createTickEntry.js
@@ -1,32 +1,37 @@
-'use strict';
+"use strict";
 
-const REGION = process.env.REGION;
 const DYNAMODB_TABLE = process.env.DYNAMODB_TABLE;
 
-const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB.DocumentClient({ region: REGION });
+const options = process.env.IS_OFFLINE
+  ? { region: "localhost", endpoint: "http://localhost:8000" }
+  : { region: process.env.REGION };
+
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB.DocumentClient(options);
 
 module.exports.handler = async (event, context) => {
   try {
     const tickEntry = {
       id: context.awsRequestId,
       data: JSON.parse(event.body),
-      createdAt: new Date().toISOString()
+      createdAt: new Date().toISOString(),
     };
 
-    await dynamodb.put({
-      TableName: DYNAMODB_TABLE,
-      Item: tickEntry
-    }).promise();
+    await dynamodb
+      .put({
+        TableName: DYNAMODB_TABLE,
+        Item: tickEntry,
+      })
+      .promise();
 
     return {
       statusCode: 201,
-      body: JSON.stringify(tickEntry)
+      body: JSON.stringify(tickEntry),
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: error.message })
+      body: JSON.stringify({ error: error.message }),
     };
   }
 };
